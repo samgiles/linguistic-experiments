685821b060a4857b597fdab8687da629ddf771c9
Sam Giles <sam.e.giles@gmail.com>
2016-05-03 15:35:32 +0100

    Comments

--- src/minimum_edit_distance.rs
+++ src/minimum_edit_distance.rs
@@ -6,19 +6,23 @@ use ndarray::OwnedArray;
 use std::cmp::min;
 use unicode_segmentation::UnicodeSegmentation;
 
+
 pub fn minimum_edit_distance(source: &str, target: &str) -> i32 {
+    // Split each word into its graphemes
     let source_graphemes = UnicodeSegmentation::graphemes(source, true).collect::<Vec<&str>>();
     let target_graphemes = UnicodeSegmentation::graphemes(target, true).collect::<Vec<&str>>();
 
     let n = source_graphemes.len() + 1;
     let m = target_graphemes.len() + 1;
 
+    // Solve as a search problem with a dynamic programming technique
     let mut d = OwnedArray::zeros((n,m));
 
     // Distance from the empty string
     d[(0, 0)] = 0;
 
 
+    // Find the minimum and maximum solutions from the empty string
     for i in 1..n {
         d[(i, 0)] =
             d[(i - 1, 0)] + deletion_cost(source_graphemes[i - 1]);
@@ -32,7 +36,7 @@ pub fn minimum_edit_distance(source: &str, target: &str) -> i32 {
     // Recurrence relation
     for i in 1..n {
         for j in 1..m {
-            println!("{} {}", i, j);
+            // Find minima
             d[(i, j)] =
                 min(d[(i - 1, j    )] + deletion_cost(source_graphemes[i - 1]),
                 min(d[(i - 1, j - 1)] + substitution_cost(source_graphemes[i - 1], target_graphemes[j - 1]),
@@ -52,6 +56,7 @@ fn deletion_cost(_: &str) -> i32 {
     1
 }
 
+// This could be weighted based on probabilities of a substitution
 fn substitution_cost(a: &str, b: &str) -> i32 {
     if a == b { 0 } else { 2 }
 }
